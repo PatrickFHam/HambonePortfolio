e02b6ee6009dfeeb838780b5238a8da6bd4a7307
PatrickFHam <93452044+PatrickFHam@users.noreply.github.com>
2022-07-30 18:34:01 -0400

    nothing works yet

---
+++ hamboneportfolio/pages/_app.js
@@ -0,0 +1,23 @@
+import '../styles/globals.css';
+import { ThemeProvider, theme, CSSReset } from '@chakra-ui/core';
+
+const customTheme = {
+  ...theme,
+  colors: {
+    ...theme.colors,
+    spacejelly: '#692ba8'
+  }
+
+};
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <ThemeProvider theme={customTheme}>
+      <CSSReset />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
+
+}
+
+export default MyApp
---
+++ hamboneportfolio/pages/gallery.js
@@ -0,0 +1,28 @@
+import Head from "next/head";
+import { Box, Text } from "@chakra-ui/core";
+
+
+export default function Gallery() {
+  return (
+    <div>
+      <Head>
+        <title> NextJS Image Gallery</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Box overflow="hidden" bg="purple.100" minH="100vh">
+        {/* <Container>
+          <Text
+            color="pink.800"
+            fontWeight="semibold"
+            mb="1rem"
+            textAlign="center"
+            textDecoration="underline"
+            fontSize={["4xl", "4xl", "5xl", "5xl"]}
+          >
+            NextJS Image Gallery
+          </Text>
+        </Container> */}
+      </Box>
+    </div>
+  );
+};
